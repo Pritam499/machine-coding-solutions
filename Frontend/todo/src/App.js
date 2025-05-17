723c9745e47f8e847d1703f32f7b40f0236f39fd
pritam499 <pritampm499@gmail.com>
2025-05-17 13:42:33 +0530

    basic react setup added

---
+++ Frontend/todo/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TodoApp from './components/TodoApp';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TodoApp />
+    </div>
+  );
+}
